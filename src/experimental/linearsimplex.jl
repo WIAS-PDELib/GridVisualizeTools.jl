19982efab8b7506271f83e1e4561edf69b6e1f90
Liameloy <liameloy@gmail.com>
2025-06-30 16:15:25 +0200

    Set-up of experimental folder Moved LinearSimplex.jl to experimental folder Created GridSimplex.jl in experimental folder

--- src/experimental/linearsimplex.jl
+++ src/experimental/linearsimplex.jl
@@ -3,7 +3,7 @@
 Linear simplex with point coordinates and node values.
 """
 struct LinearSimplex{D, N, Tv}
-    points::SVector{N, Point{D, Tv}}
+    points::SVector{N, SVector{D, Tv}}
     values::SVector{N, Tv}
 end
 
@@ -12,44 +12,44 @@ points(s::LinearSimplex) = s.points
 
 
 function LinearSimplex(::Type{Val{D}}, points::Union{Vector, Tuple}, values::Union{Vector, Tuple}) where {D}
-    spoints = SVector{D + 1, Point{D, Float32}}(points...)
+    spoints = SVector{D + 1, SVector{D, Float32}}(points...)
     svalues = SVector{D + 1, Float32}(values...)
     return LinearSimplex(spoints, svalues)
 end
 
 function LinearSimplex(::Type{Val{1}}, points::AbstractMatrix, values::AbstractVector)
-    @views spoints = SVector{2, Point{1, Float32}}(points[:, 1], points[:, 2])
+    @views spoints = SVector{2, SVector{1, Float32}}(points[:, 1], points[:, 2])
     svalues = SVector{2, Float32}(values[1], values[2])
     return LinearSimplex(spoints, svalues)
 end
 
 function LinearSimplex(::Type{Val{2}}, points::AbstractMatrix, values::AbstractVector)
-    @views spoints = SVector{3, Point{2, Float32}}(points[:, 1], points[:, 2], points[:, 3])
+    @views spoints = SVector{3, SVector{2, Float32}}(points[:, 1], points[:, 2], points[:, 3])
     svalues = SVector{3, Float32}(values[1], values[2], values[3])
     return LinearSimplex(spoints, svalues)
 end
 
 function LinearSimplex(::Type{Val{3}}, points::AbstractMatrix, values::AbstractVector)
-    @views spoints = SVector{4, Point{3, Float32}}(points[:, 1], points[:, 2], points[:, 3], points[:, 4])
+    @views spoints = SVector{4, SVector{3, Float32}}(points[:, 1], points[:, 2], points[:, 3], points[:, 4])
     svalues = SVector{4, Float32}(values[1], values[2], values[3], values[4])
     return LinearSimplex(spoints, svalues)
 end
 
 
 function LinearSimplex(::Type{Val{1}}, points::AbstractMatrix, values::AbstractVector, coordscale)
-    @views spoints = SVector{2, Point{1, Float32}}(points[:, 1] * coordscale, points[:, 2] * coordscale)
+    @views spoints = SVector{2, SVector{1, Float32}}(points[:, 1] * coordscale, points[:, 2] * coordscale)
     svalues = SVector{2, Float32}(values[1], values[2])
     return LinearSimplex(spoints, svalues)
 end
 
 function LinearSimplex(::Type{Val{2}}, points::AbstractMatrix, values::AbstractVector, coordscale)
-    @views spoints = SVector{3, Point{2, Float32}}(points[:, 1] * coordscale, points[:, 2] * coordscale, points[:, 3] * coordscale)
+    @views spoints = SVector{3, SVector{2, Float32}}(points[:, 1] * coordscale, points[:, 2] * coordscale, points[:, 3] * coordscale)
     svalues = SVector{3, Float32}(values[1], values[2], values[3])
     return LinearSimplex(spoints, svalues)
 end
 
 function LinearSimplex(::Type{Val{3}}, points::AbstractMatrix, values::AbstractVector, coordscale)
-    @views spoints = SVector{4, Point{3, Float32}}(points[:, 1] * coordscale, points[:, 2] * coordscale, points[:, 3] * coordscale, points[:, 4] * coordscale)
+    @views spoints = SVector{4, SVector{3, Float32}}(points[:, 1] * coordscale, points[:, 2] * coordscale, points[:, 3] * coordscale, points[:, 4] * coordscale)
     svalues = SVector{4, Float32}(values[1], values[2], values[3], values[4])
     return LinearSimplex(spoints, svalues)
 end
